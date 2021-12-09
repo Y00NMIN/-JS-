97ff3b58c53508a84840f2b45f35bf9200a50a83
SungMin Yoon <89017779+Y00NMIN@users.noreply.github.com>
2021-12-09 11:46:25 +0000

    update changecolor

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector("#jsCanvas");
 const ctx =canvas.getContext("2d");
+const colors = document.querySelector(".jsColor");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -33,6 +34,10 @@ function onMouseDown(event){
   painting = true;
 }
 
+function changeColorClick(event){
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
 
 if(canvas){
   canvas.addEventListener("mousemove", onMouseMove);
@@ -40,3 +45,5 @@ if(canvas){
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color=>color.addEventListner("click", changeColorClick));
